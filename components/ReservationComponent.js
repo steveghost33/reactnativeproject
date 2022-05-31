1fce6b7dfbf225f4aaef216e06499eba8b5cb61f
Steven <stevenabowman1@gmail.com>
2022-05-31 15:43:26 -0400

    Week 3 Task 2

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { Text, View, ScrollView, StyleSheet,
-    Picker, Switch, Button, Modal } from 'react-native';
+import { Text, View, ScrollView,
+    Picker, Switch, Button, StyleSheet, Alert } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 class Reservation extends Component {
@@ -14,7 +14,6 @@ class Reservation extends Component {
             hikeIn: false,
             date: new Date(),
             showCalendar: false,
-            showModal: false
         };
     }
 
@@ -22,24 +21,42 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
-    toggleModal() {
-        this.setState({showModal: !this.state.showModal});
-    }
-
     handleReservation() {
-        console.log(JSON.stringify(this.state));
-        this.toggleModal();
-    }
-
-    resetForm() {
+        const message = `Number of Campers: ${this.state.campers}
+                        \nHike-In? ${this.state.hikeIn}
+                        \nDate: ${this.state.date.toLocaleDateString("en-US")}`;
+        Alert.alert(
+          "Begin Search?",
+          message,
+          [
+            {
+              text: "Cancel",
+              onPress: () => {
+                console.log("Reservation Search Canceled");
+                this.resetForm();
+              },
+              style: "cancel",
+            },
+            {
+              text: "OK",
+              onPress: () => {
+                this.resetForm();
+              },
+            },
+          ],
+          { cancelable: false }
+        );
+      }
+    
+      resetForm() {
         this.setState({
-            campers: 1,
-            hikeIn: false,
-            date: new Date(),
-            showCalendar: false,
-            showModal: false
+          campers: 1,
+          hikeIn: false,
+          date: new Date(),
+          showCalendar: false,
         });
-    }
+      }
+    
 
     render() {
         return (
@@ -99,33 +116,6 @@ class Reservation extends Component {
                         accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
-                <Modal
-                    animationType={'slide'}
-                    transparent={false}
-                    visible={this.state.showModal}
-                    onRequestClose={() => this.toggleModal()}
-                >
-                    <View style={styles.modal}>
-                        <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                        <Text style={styles.modalText}>
-                            Number of Campers: {this.state.campers}
-                        </Text>
-                        <Text style={styles.modalText}>
-                            Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}
-                        </Text>
-                        <Text style={styles.modalText}>
-                            Date: {this.state.date.toLocaleDateString('en-US')}
-                        </Text>
-                        <Button
-                            onPress={() => {
-                                this.toggleModal();
-                                this.resetForm();
-                            }}
-                            color='#5637DD'
-                            title='Close'
-                        />
-                    </View>
-                </Modal>
                 </Animatable.View>
             </ScrollView>
         );
@@ -147,22 +137,6 @@ const styles = StyleSheet.create({
     formItem: {
         flex: 1
     },
-    modal: { 
-        justifyContent: 'center',
-        margin: 20
-    },
-    modalTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        backgroundColor: '#5637DD',
-        textAlign: 'center',
-        color: '#fff',
-        marginBottom: 20
-    },
-    modalText: {
-        fontSize: 18,
-        margin: 10
-    }
 });
 
 export default Reservation;
